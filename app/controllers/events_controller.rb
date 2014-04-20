71c2f0b1543ccfcb0376dcdd8f6777df055ef479
Sanjeev Sharma <sanjeev@sanjeev-Vostro-3560.(none)>
2014-04-20 12:12:15 +0530

    Showing event greater then current date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
 	#added destroy to above, not in Treebook
 
 	def index
-    @events = Event.order(time_begin: :asc)
+    @events = Event.where('event_start >= ?', DateTime.now).order(time_begin: :asc)
     @date_events_hash = @events.group_by{ |event| event.event_start }.sort_by{|k,v| k }.reverse
   end
 	
