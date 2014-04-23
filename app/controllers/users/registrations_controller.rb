1206699b820a8aa712d602e1ce5e3b7b49ef0b77
Christopher de Alba <superototus@gmail.com>
2014-04-22 19:21:46 -0700

    Changes to navbar (fully working, except for error page disarray) and sign up form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
-     params.require(:user).permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :profile_name, :email, :email_confirmation, :password, :remember_me)
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
 
+  validates :email, confirmation: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :profile_name, presence: true,
